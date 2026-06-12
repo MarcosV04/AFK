5ec18a3f979b3a2b17128b8e7950ba1991dbbf24
Heitor Haruki <hhassegawa@gmail.com>
2026-06-12 14:10:44 -0300

    feat:Inicio do embelezamento

---
+++ jogo/systems/cenario.py
@@ -0,0 +1,11 @@
+import pygame
+import os
+
+def load_cenario_texturas(caminho):
+    sprites = {}
+    for arquivo in os.listdir(caminho):
+        if arquivo.endswith(".png"):
+            nome = os.path.splitext(arquivo)[0]
+            img = pygame.image.load(os.path.join(caminho, arquivo)).convert_alpha()
+            sprites[nome] = img
+    return sprites
